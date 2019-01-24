8ff3980b37e62fb47ecf4b1ecae6c4152fb31243
Eli <elena.terzieva@campus.lmu.de>
2019-01-24 11:32:58 +0100

    little changes

--- cartPole.py
+++ cartPole.py
@@ -3,9 +3,11 @@ import gym
 import numpy as np
 from collections import deque
 from keras.models import *
-from keras.layers import *
-from keras.optimizers import *
-from keras import backend
+from keras.layers import Dense
+from keras.layers import Multiply
+from keras.optimizers import Adam
+
+EPISODES = 1000
 
 class DQNAgent:
     def __init__(self, state_size, action_size):
@@ -22,8 +24,6 @@ class DQNAgent:
 
     def _build_model(self):
         # Neural Net for Deep-Q learning Model
-        #model = Sequential()
-
         frames_input = Input(shape=(self.state_size,), name='frames')
         actions_input = Input((self.action_size,), name='mask')
 
@@ -40,39 +40,14 @@ class DQNAgent:
 
         return model
 
-    def huber_loss(self, y, q_value):
-        error = backend.abs(y - q_value)
-        qudratic_part = backend.clip(error, 0.0, 1.0)
-        linear_part = error - qudratic_part
-        loss = backend.mean(0.5 * backend.square(qudratic_part) + linear_part)
-        return loss
-
     def remember(self, state, action, reward, next_state, done):
         self.memory.append((state, action, reward, next_state, done))
 
     def act(self, state):
         if np.random.rand() <= self.epsilon:
             return random.randrange(self.action_size)
-        Q_values = self.model.predict([state, np.ones(self.action_size).reshape(1, action_size)])
-        return np.argmax(Q_values[0])  # returns action
-
-   # def replay(self, batch_size):
-   #     minibatch = random.sample(self.memory, batch_size)
-
-   #     for state, action, reward, next_state, done in minibatch:
-   #         target = reward
-
-    #        if not done:
-     #           target = reward + self.gamma * np.amax(self.target_model.predict(next_state)[0])
-      #      else:
-       #         target = -1
-
-        #    target_f = self.model.predict(state)
-         #   target_f[0][action] = target
-          #  self.model.fit(state, target_f, epochs=1, verbose=0)
-
-     #   if self.epsilon > self.epsilon_min:
-      #      self.epsilon *= self.epsilon_decay
+        act_values = self.model.predict([state, np.ones(self.action_size).reshape(1, action_size)])
+        return np.argmax(act_values[0])  # returns action
 
     def get_sample_random_batch_from_replay_memory(self):
         mini_batch = random.sample(self.memory, batch_size)
@@ -91,10 +66,7 @@ class DQNAgent:
 
         return current_state_batch, actions, rewards, next_state_batch, dead
 
-    def replay_2(self, batch_size):
-        if len(self.memory) < batch_size:
-            return
-
+    def replay(self, batch_size):
         state, action, reward, next_state, done = self.get_sample_random_batch_from_replay_memory()
 
         action_mask = np.ones((batch_size, self.action_size))
@@ -120,41 +92,39 @@ class DQNAgent:
     def set_weights(self):
         self.target_model.set_weights(self.model.get_weights())
 
-if __name__ == "__main__":
-    episodes = 1000
-    batch_size = 32
-    target_model_change = 100
 
-    env = gym.make('CartPole-v0')
+if __name__ == "__main__":
+    env = gym.make('CartPole-v1')
 
     state_size = env.observation_space.shape[0]
     action_size = env.action_space.n
+
     agent = DQNAgent(state_size, action_size)
 
-    # Iterate the game
-    for episode in range(episodes):
-        # reset state in the beginning of each game
+    done = False
+    batch_size = 32
+
+    for episode in range(EPISODES):
         state = env.reset()
-        env.render()
         state = np.reshape(state, [1, state_size])
 
-        for time_t in range(500):
+        for time in range(500):
+            # env.render()
             action = agent.act(state)
-
             next_state, reward, done, _ = env.step(action)
-            next_state = np.reshape(next_state, [1, state_size])
 
+            next_state = np.reshape(next_state, [1, state_size])
             agent.remember(state, action, reward, next_state, done)
 
             state = next_state
 
             if done:
-                print("episode: {}, score: {}, e: {:.2}"
-                      .format(episode, time_t, agent.epsilon))
+                print("episode: {}/{}, score: {}, e: {:.2}"
+                      .format(episode, EPISODES, time, agent.epsilon))
                 break
 
-            # train the agent with the experience of the episode
-            agent.replay_2(batch_size)
+            if len(agent.memory) > batch_size:
+                agent.replay(batch_size)
 
             if episode % 2 == 0:
                 agent.set_weights()
--- cartPole_simpleTarget.py
+++ cartPole_simpleTarget.py
@@ -2,9 +2,11 @@ import random
 import gym
 import numpy as np
 from collections import deque
-from keras.models import *
-from keras.layers import *
-from keras.optimizers import *
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.optimizers import Adam
+
+EPISODES = 1000
 
 class DQNAgent:
     def __init__(self, state_size, action_size):
@@ -35,68 +37,54 @@ class DQNAgent:
     def act(self, state):
         if np.random.rand() <= self.epsilon:
             return random.randrange(self.action_size)
-        Q_values = self.model.predict(state)
-        return np.argmax(Q_values[0])  # returns action
+        act_values = self.model.predict(state)
+        return np.argmax(act_values[0])  # returns action
 
     def replay(self, batch_size):
-        if len(self.memory) < batch_size:
-            return
-
         minibatch = random.sample(self.memory, batch_size)
-
         for state, action, reward, next_state, done in minibatch:
             target = reward
-
             if not done:
-                target = reward + self.gamma * np.amax(self.target_model.predict(next_state)[0])
-            else:
-                target = -1
-
+                target = (reward + self.gamma *
+                          np.amax(self.target_model.predict(next_state)[0]))
             target_f = self.model.predict(state)
             target_f[0][action] = target
             self.model.fit(state, target_f, epochs=1, verbose=0)
-
         if self.epsilon > self.epsilon_min:
             self.epsilon *= self.epsilon_decay
 
     def set_weights(self):
         self.target_model.set_weights(self.model.get_weights())
 
+
 if __name__ == "__main__":
-    episodes = 1000
-    batch_size = 32
-    target_model_change = 100
-
-    env = gym.make('CartPole-v0')
-
+    env = gym.make('CartPole-v1')
     state_size = env.observation_space.shape[0]
     action_size = env.action_space.n
     agent = DQNAgent(state_size, action_size)
+    # agent.load("./save/cartpole-dqn.h5")
+    done = False
+    batch_size = 32
 
-    # Iterate the game
-    for episode in range(episodes):
-        # reset state in the beginning of each game
+    for e in range(EPISODES):
         state = env.reset()
-        env.render()
         state = np.reshape(state, [1, state_size])
-
-        for time_t in range(500):
+        for time in range(500):
+            # env.render()
             action = agent.act(state)
-
             next_state, reward, done, _ = env.step(action)
+            reward = reward if not done else -10
             next_state = np.reshape(next_state, [1, state_size])
-
             agent.remember(state, action, reward, next_state, done)
-
             state = next_state
-
             if done:
-                print("episode: {}, score: {}, e: {:.2}"
-                      .format(episode, time_t, agent.epsilon))
+                print("episode: {}/{}, score: {}, e: {:.2}"
+                      .format(e, EPISODES, time, agent.epsilon))
                 break
+            if len(agent.memory) > batch_size:
+                agent.replay(batch_size)
 
-            # train the agent with the experience of the episode
-            agent.replay(batch_size)
-
-            if episode % 2 == 0:
+            if e % 2 == 0:
                 agent.set_weights()
+        # if e % 10 == 0:
+        #     agent.save("./save/cartpole-dqn.h5")
